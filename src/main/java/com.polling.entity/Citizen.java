236bf805209a593c0831f8293256036b3aef91ae
Swapnil Shukla <54302125+ShuklaSwapnil04@users.noreply.github.com>
2020-10-21 02:41:11 -0700

    Add files via upload
    entity commit

---
+++ src/main/java/com.polling.entity/Citizen.java
@@ -0,0 +1,42 @@
+package com.Polling.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name="citizens")
+@Entity
+public class Citizen {
+
+	@Id
+	@Column(name="id")
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Column(name="citizen_name")
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Citizen(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	
+}

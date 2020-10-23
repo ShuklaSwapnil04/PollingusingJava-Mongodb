a3aae3c53421556dbc55830e1f6754188629b6ea
Swapnil Shukla <54302125+ShuklaSwapnil04@users.noreply.github.com>
2020-10-23 04:56:02 -0700

    Add files via upload
    main method commit

---
+++ src/main/java/com.polling/PollingSystemJavaMySqlApplication.java
@@ -0,0 +1,15 @@
+package com.Polling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class PollingSystemJavaMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingSystemJavaMySqlApplication.class, args);
+	}
+
+}
